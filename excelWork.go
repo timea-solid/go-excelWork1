cec4a2d2179950ebda7d90b8ec400eb5ef31e46b
Timea Turdean <t.turdean@semantic-web.at>
2017-12-06 17:35:04 +0100

    improved for loop

--- excelWork.go
+++ excelWork.go
@@ -14,21 +14,28 @@ func check(e error) {
 
 func main() {
 	var sheet *xlsx.Sheet
+	
 	excelFile := "D:/SemanticWeb/MyClients&Projects/VirtualAssistant/Content/InputDataDE_FinServ2Click.xlsx"
 	outputFilePath := "D:/SemanticWeb/MyClients&Projects/VirtualAssistant/Content/InputDataDE_FinServ2Click"
+	
+//	excelFile := os.Args[1]
+//	outputFilePath := os.Args[2] 
+//	columnsToConcat := os.Args[3:]
+	
+	
 	xlsxFile, err := xlsx.OpenFile(excelFile)
 	if err != nil {
 		fmt.Printf("there was an error\n")
 	}
 	sheet = xlsxFile.Sheets[6]
-	index := 0
-	for _, row := range sheet.Rows {
-		if index >= 1 {
+//	index := 2
+	for i, row := range sheet.Rows {
+		if i >= 2 {
 			question := row.Cells[12].String()
 			answer := row.Cells[18].String()
 			hint := row.Cells[20].String()
 //			fmt.Printf("%s\n %s\n %s\n",question, answer, hint)
-			fileName := fmt.Sprintf("%s%s%d%s",outputFilePath,  "/", (index+1), ".txt")
+			fileName := fmt.Sprintf("%s%s%d%s",outputFilePath,  "/", i, ".txt")
 			f, err := os.Create(fileName)
 			check(err)
 			f.WriteString(question)
@@ -38,6 +45,5 @@ func main() {
 			f.WriteString(hint)
 			f.Sync()
 		}
-		index ++ 
 	}
 }
